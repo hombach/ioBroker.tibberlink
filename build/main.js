7eec57d40d1cdbe116e384037a342092f4ff84c8
hombach <63002482+hombach@users.noreply.github.com>
2023-07-15 17:50:51 +0200

    sentry

--- build/main.js
+++ build/main.js
@@ -94,23 +94,23 @@ class Tibberlink extends utils.Adapter {
                 }
             }
             // sentry.io ping
-            if (this.supportsFeature && this.supportsFeature('PLUGINS')) {
-                const sentryInstance = this.getPluginInstance('sentry');
+            if (this.supportsFeature && this.supportsFeature("PLUGINS")) {
+                const sentryInstance = this.getPluginInstance("sentry");
                 const today = new Date();
                 //var last = await this.getStateAsync('LastSentryLogDay')
                 //if (last?.val != await today.getDate()) {
                 if (sentryInstance) {
                     const Sentry = sentryInstance.getSentryObject();
                     Sentry && Sentry.withScope((scope) => {
-                        scope.setLevel('info');
-                        scope.setTag('SentryDay', today.getDate());
-                        scope.setTag('HomeIDs', this.homeIdList.length);
-                        Sentry.captureMessage('Adapter TibberLink started', 'info'); // Level "info"
+                        scope.setLevel("info");
+                        scope.setTag("SentryDay", today.getDate());
+                        scope.setTag("HomeIDs", this.homeIdList.length);
+                        Sentry.captureMessage("Adapter TibberLink started", "info"); // Level "info"
                     });
                 }
-                //this.setStateAsync('LastSentryLoggedError', { val: 'unknown', ack: true }); // Clean last error every adapter start
-                //this.setStateAsync('LastSentryLogDay', { val: today.getDate(), ack: true });
-                //}
+                // this.setStateAsync("LastSentryLoggedError", { val: "unknown", ack: true }); // Clean last error every adapter start
+                // this.setStateAsync("LastSentryLogDay", { val: today.getDate(), ack: true });
+                // }
             }
             // Init Load Data for home
             if (this.homeIdList.length > 0) {
--- src/main.ts
+++ src/main.ts
@@ -74,23 +74,23 @@ class Tibberlink extends utils.Adapter {
 			}
 
 			// sentry.io ping
-			if (this.supportsFeature && this.supportsFeature('PLUGINS')) {
-				const sentryInstance = this.getPluginInstance('sentry');
+			if (this.supportsFeature && this.supportsFeature("PLUGINS")) {
+				const sentryInstance = this.getPluginInstance("sentry");
 				const today = new Date();
 				//var last = await this.getStateAsync('LastSentryLogDay')
 				//if (last?.val != await today.getDate()) {
-					if (sentryInstance) {
-						const Sentry = sentryInstance.getSentryObject();
-						Sentry && Sentry.withScope((scope: { setLevel: (arg0: string) => void; setTag: (arg0: string, arg1: number) => void; }) => {
-							scope.setLevel('info');
-							scope.setTag('SentryDay', today.getDate());
-							scope.setTag('HomeIDs', this.homeIdList.length);
-							Sentry.captureMessage('Adapter TibberLink started', 'info'); // Level "info"
-						});
-					}
-					//this.setStateAsync('LastSentryLoggedError', { val: 'unknown', ack: true }); // Clean last error every adapter start
-					//this.setStateAsync('LastSentryLogDay', { val: today.getDate(), ack: true });
-				//}
+				if (sentryInstance) {
+					const Sentry = sentryInstance.getSentryObject();
+					Sentry && Sentry.withScope((scope: { setLevel: (arg0: string) => void; setTag: (arg0: string, arg1: number) => void; }) => {
+						scope.setLevel("info");
+						scope.setTag("SentryDay", today.getDate());
+						scope.setTag("HomeIDs", this.homeIdList.length);
+						Sentry.captureMessage("Adapter TibberLink started", "info"); // Level "info"
+					});
+				}
+				// this.setStateAsync("LastSentryLoggedError", { val: "unknown", ack: true }); // Clean last error every adapter start
+				// this.setStateAsync("LastSentryLogDay", { val: today.getDate(), ack: true });
+				// }
 			}
 
 			// Init Load Data for home
