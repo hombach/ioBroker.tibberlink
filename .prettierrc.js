ec7c2c845c22a9601a537d73ee4e7f9b4c9bf52e
hombach <63002482+hombach@users.noreply.github.com>
2023-09-03 21:32:40 +0200

    0.2.6

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
 	semi: true,
 	trailingComma: "all",
 	singleQuote: false,
-	printWidth: 160,
+	printWidth: 175,
 	useTabs: true,
 	tabWidth: 4,
 	endOfLine: "lf",
--- src/lib/tibberCalculator.ts
+++ src/lib/tibberCalculator.ts
@@ -8,11 +8,7 @@ export class TibberCalculator extends TibberHelper {
 
 	async setupCalculatorStates(homeId: string, channel: number): Promise<void> {
 		try {
-			this.checkAndSetValue(
-				this.getStatePrefix(homeId, `Calculations.${channel}`, "TargetState"),
-				"EMPTY",
-				"target state to write triggered values of this channel",
-			);
+			this.checkAndSetValue(this.getStatePrefix(homeId, `Calculations.${channel}`, "TargetState"), "EMPTY", "target state to write triggered values of this channel");
 			this.checkAndSetValueNumber(this.getStatePrefix(homeId, `Calculations.${channel}`, "TriggerPrice"), 0.0, "pricelevel to trigger this channel at");
 			this.checkAndSetValueBoolean(this.getStatePrefix(homeId, `Calculations.${channel}`, "Active"), false, "Whether the calculation channel is active");
 		} catch (error) {
