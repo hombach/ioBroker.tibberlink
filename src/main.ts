e52c05295582a9b9c8b18b6a59f2eaf3394c38a4
hombach <63002482+hombach@users.noreply.github.com>
2023-09-13 21:36:57 +0200

    0.3.1

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { IConfig } from "tibber-api";
 import { TibberAPICaller } from "./lib/tibberAPICaller";
 import { TibberPulse } from "./lib/tibberPulse";
 import { TibberCalculator } from "./lib/tibberCalculator";
-import { isNullOrUndefined } from "node:util";
 
 class Tibberlink extends utils.Adapter {
 	intervalList: any[]; // intervalList: ioBroker.Interval[]; - - ERROR not working with adapter-core 3.x; has to be any
