8f2e52694d0796a1a5eae386e805248d37e161f4
Hombach <GIT@homba.ch>
2024-05-20 21:57:33 +0200

    3.1.2

--- src/main.ts
+++ src/main.ts
@@ -233,18 +233,13 @@ class Tibberlink extends utils.Adapter {
 					const tibberPulseInstances = new Array(this.homeInfoList.length); // array for TibberPulse-instances
 
 					if (!this.homeInfoList.some((homeInfo) => homeInfo.ID == `None available - restart adapter after entering token`)) {
-						const stateObject = await this.getObjectAsync(`Homes.None available - restart adapter after entering token`);
-						if (!stateObject) {
-							this.log.debug(`Temp home channel from adapter setup already deleted`);
-						} else {
-							this.delObjectAsync(`Homes.None available - restart adapter after entering token`, { recursive: true })
-								.then(() => {
-									this.log.debug(`Deleted temp home channel from adapter setup`);
-								})
-								.catch((error) => {
-									this.log.error(`Error deleting temp home channel from adapter setup: ${error}`);
-								});
-						}
+						//const stateObject = await this.getObjectAsync(`Homes.None available - restart adapter after entering token`);
+						//if (!stateObject) {
+						//this.log.debug(`Temp home channel from adapter setup already deleted`);
+						//} else {
+						this.delObjectAsync(`Homes.None available - restart adapter after entering token`, { recursive: true });
+						//this.log.debug(`Deleted temp home channel from adapter setup`);
+						//}
 					}
 
 					for (const index in this.homeInfoList) {
