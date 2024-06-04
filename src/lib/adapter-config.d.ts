6fec19ca44e76d4d17f3085797bb482dd31b3824
Hombach <GIT@homba.ch>
2024-06-04 19:37:53 +0200

    3.2.2

--- src/lib/adapter-config.d.ts
+++ src/lib/adapter-config.d.ts
@@ -43,6 +43,7 @@ declare global {
 			FeedConfigCurrentL3: boolean;
 			FeedConfigSignalStrength: boolean;
 			UseCalculator: boolean;
+			UseObsoleteStats: boolean;
 			CalculatorList: [
 				{
 					chHomeID: string;
