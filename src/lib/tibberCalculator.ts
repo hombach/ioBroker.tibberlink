86db8901099b0f33ee35291b553d0a084ac52137
Hombach <GIT@homba.ch>
2024-11-04 23:35:47 +0100

    3.5.3

--- src/lib/tibberCalculator.ts
+++ src/lib/tibberCalculator.ts
@@ -215,9 +215,11 @@ export class TibberCalculator extends ProjectUtils {
 
 			//#region *** subscribe state changes ***
 			// this.adapter.subscribeStates(`Homes.${homeId}.Calculations.${channel}.*`);
-			this.adapter.subscribeStates(`Homes.${homeId}.Calculations.${channel}.Active`);
+			//this.adapter.subscribeStates(`Homes.${homeId}.Calculations.${channel}.Active`);
+			this.adapter.subscribeStates(`Homes.${homeId}.Calculations.${channel.toString}.Active`);
 			this.adapter.subscribeStates(`Homes.${homeId}.Calculations.${channel}.TriggerPrice`);
-			this.adapter.subscribeStates(`Homes.${homeId}.Calculations.${channel}.AmountHours`);
+			//this.adapter.subscribeStates(`Homes.${homeId}.Calculations.${channel}.AmountHours`);
+			this.adapter.subscribeStates(`Homes.${homeId}.Calculations.${channel.toString}.AmountHours`);
 			this.adapter.subscribeStates(`Homes.${homeId}.Calculations.${channel}.StartTime`);
 			this.adapter.subscribeStates(`Homes.${homeId}.Calculations.${channel}.StopTime`);
 			this.adapter.subscribeStates(`Homes.${homeId}.Calculations.${channel}.RepeatDays`);
