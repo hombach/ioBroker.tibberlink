5a739963357ffe8e5ef8b29f2f10a5f08993e585
Hombach <GIT@homba.ch>
2024-12-01 21:18:33 +0100

    ESM4.0.0

--- test/integration.js
+++ test/integration.js
@@ -3,5 +3,5 @@ import { tests } from "@iobroker/testing";
 
 // Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
 tests.integration(process.cwd(), {
-	allowedExitCodes: [11, 1],
+	allowedExitCodes: [0],
 });
