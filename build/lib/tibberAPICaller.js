41ed44473811bde6bfb07b0804d9711d369c48ad
Hombach <GIT@homba.ch>
2024-12-29 16:56:43 +0100

    4.2.0

--- build/lib/tibberAPICaller.js
+++ build/lib/tibberAPICaller.js
@@ -517,29 +517,29 @@ class TibberAPICaller extends projectUtils_1.ProjectUtils {
         console.log("Starts At Values:", startsAtValues);
         //const jsonFlexCharts = `Hello World ${JSON.stringify(startsAtValues)} - ${JSON.stringify(totalValues)}`;
         const jsonFlexCharts = `option = {
-			backgroundColor: 'rgb(220, 220, 220)',
+			backgroundColor: "rgb(232, 232, 232)",
 			title: {
-				text: 'Tibber Price',
+				text: "Tibber Price",
 			},
 			tooltip: {
-				trigger: 'axis',
+				trigger: "axis",
 				axisPointer: {
-					type: 'cross'
+					type: "cross"
 				}
 			},
 			grid: { // Randabstände
-				left: '10%', right: '4%', top: '8%', bottom: '8%'
+				left: "10%", right: "4%", top: "8%", bottom: "8%"
 			},
 			xAxis: {
-				type: 'category',
+				type: "category",
 				boundaryGap: false,
 				data: ${JSON.stringify(startsAtValues)}.map(function (str) {
-				return str.replace('T', '\n');
+				return str.replace("T", "\n");
 				})
 			},
 			yAxis: {
-				type: 'value',
-				axisLabel: {formatter: '{value} ct/kWh'},
+				type: "value",
+				axisLabel: {formatter: "{value} ct/kWh"},
 				axisPointer: {
 					snap: true
 				}
@@ -548,25 +548,25 @@ class TibberAPICaller extends projectUtils_1.ProjectUtils {
 				min: 0.2,
 				max: 0.3,
 				inRange: {
-					color: ['green', 'yellow', 'red'] // Verlauf von grün über gelb nach rot
+					color: ["green", "yellow", "red"] // Verlauf von grün über gelb nach rot
 				},
 				show: false
 			},
 			series: [
 				{
-					name: 'Total',
-					type: 'line',
-					step: 'end',
-					symbol: 'none',
+					name: "Total",
+					type: "line",
+					step: "end",
+					symbol: "none",
 					data: ${JSON.stringify(totalValues)},
 
 					markArea: {
 						itemStyle: {
-							color: 'rgba(120, 200, 120, 0.2)'
+							color: "rgba(120, 200, 120, 0.2)"
 						},
 						data: [
-							[{name: 'Car Charging', xAxis: '29.12.\n04:00'}, {xAxis: '29.12.\n07:00'}],
-							[{name: 'Battery', xAxis: '29.12.\n21:00'}, {xAxis: '29.12.\n24:00'}]
+							[{name: "Car Charging", xAxis: "29.12.\n04:00"}, {xAxis: "29.12.\n07:00"}],
+							[{name: "Battery", xAxis: "29.12.\n21:00"}, {xAxis: "29.12.\n24:00"}]
 						]
 					}
 				}
