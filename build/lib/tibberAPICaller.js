6c5b17ce781893a533e6983eed4e787e383fd063
Hombach <GIT@homba.ch>
2024-12-29 23:44:27 +0100

    4.2.0

--- build/lib/tibberAPICaller.js
+++ build/lib/tibberAPICaller.js
@@ -511,64 +511,6 @@ class TibberAPICaller extends projectUtils_1.ProjectUtils {
                 const date = new Date(item.startsAt);
                 return (0, date_fns_1.format)(date, "dd.MM.'T'HH:mm");
             });
-            /*
-            let jsonFlexCharts = `option = {
-                backgroundColor: "rgb(232, 232, 232)",
-                title: {
-                    text: "Tibber Price",
-                },
-                tooltip: {
-                    trigger: "axis",
-                    axisPointer: {
-                        type: "cross"
-                    }
-                },
-                grid: { // Randabstände
-                    left: "10%", right: "4%", top: "8%", bottom: "8%"
-                },
-                xAxis: {
-                    type: "category",
-                    boundaryGap: false,
-                    data: %%xAxisData%%$.map(function (str) {
-                    return str.replace("T", "\\n"); // doppelter Backslash nötig
-                    })
-                },
-                yAxis: {
-                    type: "value",
-                    axisLabel: {formatter: "{value} ct/kWh"},
-                    axisPointer: {
-                        snap: true
-                    }
-                },
-                visualMap: {
-                    min: 0.2,
-                    max: 0.3,
-                    inRange: {
-                        color: ["green", "yellow", "red"] // Verlauf von grün über gelb nach rot
-                    },
-                    show: false
-                },
-                series: [
-                    {
-                        name: "Total",
-                        type: "line",
-                        step: "end",
-                        symbol: "none",
-                        data: %%yAxisData%%,
-
-                        markArea: {
-                            itemStyle: {
-                                color: "rgba(120, 200, 120, 0.2)"
-                            },
-                            data: [
-                                [{name: "Car Charging", xAxis: "29.12.\\n04:00"}, {xAxis: "29.12.\\n07:00"}], // doppelter Backslash nötig
-                                [{name: "Battery", xAxis: "29.12.\\n21:00"}, {xAxis: "30.12.\\n00:00"}] // doppelter Backslash nötig
-                            ]
-                        }
-                    }
-                ]
-            };`;
-            */
             //#region *** FlexCharts demo
             /*
             option = {
